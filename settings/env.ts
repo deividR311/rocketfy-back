8a37275d819fcee5613f07ab4e4c54e3510ce0f9
deividR311 <deicen24@gmail.com>
2023-10-17 14:42:05 -0500

    DEPLOY: fix file name errors

--- settings/env.ts
+++ settings/env.ts
@@ -1,5 +1,5 @@
 export const EnvVariables = {
     PORT: process.env.PORT as any | 3000,
     PRODUCTION: process.env.PRODUCTION as any | false,
-    DB_HOST: process.env.DB_HOST as any | 'mongodb://127.0.0.1:27017/Rocketfy'
+    DB_HOST: process.env.DB_HOST as any | 'mongodb+srv://rocketfy:rocketfy@rocketfy.bmnkrxd.mongodb.net/?retryWrites=true&w=majority'
 };
